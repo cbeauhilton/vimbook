b7467e8d016e41de2ae8444c72f1e2c6207206ee
Beau <cbeauhilton@gmail.com>
2021-09-30 11:30:38 -0500

    working

--- get_links.py
+++ get_links.py
@@ -1,5 +1,7 @@
 from requests_html import HTMLSession
-from urllib.parse import quote_plus
+from bs4 import BeautifulSoup
+import pandas as pd
+import pypandoc
 
 session = HTMLSession()
 
@@ -7,7 +9,7 @@ session = HTMLSession()
 def get_top_links(url: str):
 
     r = session.get(url)
-    links = [link for link in r.html.absolute_links]
+    links = [link for link in r.html.absolute_links if "commodorecompendium" in link]
 
     return links
 
@@ -15,7 +17,7 @@ def get_top_links(url: str):
 def get_subject_links(url: str):
     r = session.get(url)
     subjects = r.html.find("#content", first=True)
-    links = [link for link in subjects.absolute_links]
+    links = [link for link in subjects.absolute_links if "commodorecompendium" in link]
     return links
 
 
@@ -25,15 +27,58 @@ def get_html(url: str):
     return content.html
 
 
+def convert_table(table):
+    df = pd.read_html(str(table))[0]
+    table.string = df.to_html(index=False)
+    return table
+
+
 links = get_top_links("https://medsites.vumc.org/commodorecompendium/introduction")
 
 for link in links:
     try:
         subjects = get_subject_links(link)
         for subject in subjects:
-                content = get_html(subject)
-                file_name = f"content/{quote_plus(subject)}"
-                with open(file_name, "w") as f:
-                    f.write(content)
+            try:
+                print("\n", subject)
+                res = get_html(subject)
+                soup = BeautifulSoup(res, "lxml")
+
+                try:
+                    tables = soup.find_all("table")
+                    for table in tables:
+                        convert_table(table)
+                except Exception as exc:
+                    print(f"no table in {subject}, {exc}")
+
+                for data in soup(["script", "head"]):
+                    data.decompose()
+                soup = soup.prettify(formatter=None)
+
+                file_name = f"{link.split('/')[-1]}-{subject.split('/')[-1]}"
+                html_file = f"content/html/{file_name}.html"
+
+                md_file = f"content/md/{file_name}.md"
+                with open(html_file, "w", encoding="utf-8") as f:
+                    f.write(str(soup))
+
+                output = pypandoc.convert_file(
+                    source_file=html_file,
+                    format="html-native_divs-native_spans",
+                    to="gfm",
+                    outputfile=md_file,
+                )
+
+                # make first line a # header so bookdown will work
+                with open(md_file) as f:
+                    firstline = f.readline()
+                    lines = f.readlines()
+                lines[0] = f"# {firstline} \n"
+                with open(md_file, "w") as f:
+                    f.writelines(lines)
+
+            except Exception as exc:
+                print(f"no table in {subject}, {exc}")
+
     except Exception as exc:
         print(exc)
